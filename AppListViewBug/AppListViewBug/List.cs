5f07356d0323cb1283b58b62267477ba1f41e137
Андрей <@W7>
2019-12-28 11:15:12 +0300

    Add project files.

---
+++ AppListViewBug/AppListViewBug/List.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AppListViewBug
+{
+    internal class List : List<string>
+    {
+        public List(int capacity) : base(capacity)
+        {
+        }
+    }
+}
---
+++ AppListViewBug/AppListViewBug/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppListViewBug
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            myListStack.ItemsSource = myListGrid.ItemsSource = myListGridChanging.ItemsSource = GetItems();
+        }
+
+
+        public double myWidth
+        {
+            get { return (double)GetValue(myWidthProperty); }
+            set { SetValue(myWidthProperty, value); }
+        }
+        public static readonly BindableProperty myWidthProperty = BindableProperty.Create("myWidth", typeof(double), typeof(ContentPage), default(double));
+
+        List<string> items = new List<string>();
+
+        IEnumerable<string> GetItems()
+        {
+            for (int i=0; i<50; i++)
+            {
+                items.Add("item: " + i);
+            }
+
+            return items;
+        }
+
+        private void ContentPage_SizeChanged(object sender, EventArgs e)
+        {
+            Debug.WriteLine(Width + " " + Height);
+            myWidth = Width - 100;
+        }
+    }
+}
